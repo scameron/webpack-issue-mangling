fec115ff25c4ed8d0fca711d9f0695da2370bfa7
Scott Cameron <scott.cameron@sap.com>
2025-02-07 09:52:09 +0100

    Simplify Consolidator

--- src/Consolidator.ts
+++ src/Consolidator.ts
@@ -1,7 +1,3 @@
-import * as MyDirectEnums from './Enums';
 import * as MyProvidedEnums from './Provider'
-
-export import MyEnum = MyDirectEnums.MyEnum;
-export import MyEnum2 = MyDirectEnums.MyEnum2;
 
 export import getEnums = MyProvidedEnums.getEnums;
